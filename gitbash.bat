127bffd26f30ecb9aef1349f606e00be0a45f8ff
Thai Son <matxichtuthan@gmail.com>
2025-03-02 19:32:05 +0700

    update

--- gitbash.bat
+++ gitbash.bat
@@ -3,4 +3,4 @@ git config --global user.name "Thai Son"
 git config --global user.email "matxichtuthan@gmail.com"
 git add .
 git commit -m "update"
-git push origin master
+git push origin master --force
